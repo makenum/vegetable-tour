646c7332cc5c33229ead1037ff836d8c839abd08
makenum <dotdesgin@gmail.com>
2019-09-18 15:52:02 +0800

    蔬游第二版，新提交。

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,13 @@
-$(function () {
+$(function() {
   // 头部导航固定
-  $(".headbar-sticky").on("sticky-start", function () {
+  $(".headbar-sticky").on("sticky-start", function() {
     if ($(".aiui-headbar").hasClass("aiui-headbar__transparent")) {
       $(".aiui-headbar")
         .removeClass("aiui-headbar__transparent")
         .addClass("aiui-headbar__default");
     }
   });
-  $(".headbar-sticky").on("sticky-end", function () {
+  $(".headbar-sticky").on("sticky-end", function() {
     if ($(".aiui-headbar").hasClass("aiui-headbar__default")) {
       $(".aiui-headbar")
         .removeClass("aiui-headbar__default")
@@ -17,7 +17,7 @@ $(function () {
   // 数字增减器
   var MIN = 1;
   $(".ui-count_decrease").attr("disabled", "disabled");
-  $(".ui-count_decrease").click(function (e) {
+  $(".ui-count_decrease").click(function(e) {
     var $input = $(e.currentTarget)
       .parent()
       .find(".ui-count_number");
@@ -28,7 +28,7 @@ $(function () {
     }
     $input.val(number);
   });
-  $(".ui-count_increase").click(function (e) {
+  $(".ui-count_increase").click(function(e) {
     $(this)
       .siblings(".ui-count_decrease")
       .removeAttr("disabled");
@@ -38,5 +38,15 @@ $(function () {
     var number = parseInt($input.val()) + 1;
     $input.val(number);
   });
-  // 发送验证码
+  // 返回顶部
+  $(window).scroll(function() {
+    if ($(window).scrollTop() >= 100) {
+      $(".aiui-gotop-hook").fadeIn(300);
+    } else {
+      $(".aiui-gotop-hook").fadeOut(300);
+    }
+  });
+  $(".aiui-gotop-hook").bind("click", function() {
+    $("html,body").animate({ scrollTop: 0 }, 300);
+  });
 });
